adf5209144283a6e912a791dd52f1eaa2074462a
leeseungmin0 <tmdalsdl112@naver.com>
2022-02-22 19:06:48 +0900

    JS -  plus,minus,multiply

---
+++ main.js
@@ -0,0 +1,17 @@
+// 더하기
+function plus(a,b,c){
+    console.log(a+b+c);
+}
+plus(6,7,14);
+
+// 빼기
+function minus (d,e,f){
+    console.log(d-e-f);
+}
+minus (20,10,3)
+
+// 곱하기
+function multiply (g,h){
+    console.log(g*h);
+}
+multiply (2,24)
